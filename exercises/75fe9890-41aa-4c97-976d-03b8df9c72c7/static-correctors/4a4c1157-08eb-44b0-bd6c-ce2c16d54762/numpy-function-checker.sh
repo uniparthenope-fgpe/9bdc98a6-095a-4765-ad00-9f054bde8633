7e667e68796511aaf2cd2afaed3660b892b37d20
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2023-03-10 11:11:03 +0100

    created file exercises/75fe9890-41aa-4c97-976d-03b8df9c72c7/static-correctors/4a4c1157-08eb-44b0-bd6c-ce2c16d54762/numpy-function-checker.sh

---
+++ exercises/75fe9890-41aa-4c97-976d-03b8df9c72c7/static-correctors/4a4c1157-08eb-44b0-bd6c-ce2c16d54762/numpy-function-checker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set the file name and library name variables
+file_name=$1
+function_name=$2
+library_name="numpy"
+
+# Use grep to search for the library import statement in the file
+if grep -q "import $library_name" "$file_name" && grep -q "$function_name" "$file_name"; then
+    echo "Correct function"
+    exit 0
+else
+    exit 2
+fi
