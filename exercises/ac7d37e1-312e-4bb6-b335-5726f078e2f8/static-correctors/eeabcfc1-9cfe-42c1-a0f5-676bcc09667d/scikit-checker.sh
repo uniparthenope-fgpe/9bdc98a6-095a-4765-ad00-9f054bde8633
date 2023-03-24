7fe68934377b76ba44b5a6d7d085d35acb89ae3a
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2023-03-24 13:33:18 +0100

    created file exercises/ac7d37e1-312e-4bb6-b335-5726f078e2f8/static-correctors/eeabcfc1-9cfe-42c1-a0f5-676bcc09667d/scikit-checker.sh

---
+++ exercises/ac7d37e1-312e-4bb6-b335-5726f078e2f8/static-correctors/eeabcfc1-9cfe-42c1-a0f5-676bcc09667d/scikit-checker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set the file name and library name variables
+file_name=$1
+library_name="sklearn"
+
+# Use grep to search for the library import statement in the file
+if grep -q "import $library_name" "$file_name"; then
+    echo "The file $file_name imports the $library_name library."
+    exit 0
+else
+    echo "The file $file_name does not import the $library_name library."
+    exit 2
+fi
