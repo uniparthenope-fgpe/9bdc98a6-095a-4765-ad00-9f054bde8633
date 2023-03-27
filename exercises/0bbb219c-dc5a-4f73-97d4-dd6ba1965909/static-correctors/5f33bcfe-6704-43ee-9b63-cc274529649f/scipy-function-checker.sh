2c75d3aa419ba99a88a28248ddf401dbed0b1295
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2023-03-27 11:38:23 +0200

    updated file exercises/0bbb219c-dc5a-4f73-97d4-dd6ba1965909/static-correctors/5f33bcfe-6704-43ee-9b63-cc274529649f/scipy-function-checker.sh

--- exercises/0bbb219c-dc5a-4f73-97d4-dd6ba1965909/static-correctors/5f33bcfe-6704-43ee-9b63-cc274529649f/scipy-function-checker.sh
+++ exercises/0bbb219c-dc5a-4f73-97d4-dd6ba1965909/static-correctors/5f33bcfe-6704-43ee-9b63-cc274529649f/scipy-function-checker.sh
@@ -6,7 +6,7 @@ function_name=$2
 library_name="scipy"
 
 # Use grep to search for the library import statement in the file
-if grep -q "import $library_name" "$file_name" && grep -q "$function_name" "$file_name"; then
+if grep -q "$library_name" "$file_name" && grep -q "$function_name" "$file_name"; then
     echo "Correct function"
     exit 0
 else
