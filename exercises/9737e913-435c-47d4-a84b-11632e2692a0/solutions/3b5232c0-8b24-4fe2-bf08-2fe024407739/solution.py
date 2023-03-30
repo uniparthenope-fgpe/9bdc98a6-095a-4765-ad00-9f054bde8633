3eef2c46504062d7dcd0c8399b88168576217da2
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2023-03-30 11:15:45 +0200

    created file exercises/9737e913-435c-47d4-a84b-11632e2692a0/solutions/3b5232c0-8b24-4fe2-bf08-2fe024407739/solution.py

---
+++ exercises/9737e913-435c-47d4-a84b-11632e2692a0/solutions/3b5232c0-8b24-4fe2-bf08-2fe024407739/solution.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+
+# Load the diabetes dataset
+diabetes = load_diabetes()
+
+# Split the dataset into a training set and a testing set
+X_train, X_test, y_train, y_test = train_test_split(diabetes.data, diabetes.target, test_size=0.2, random_state=42)
+
+# Create a Linear Regression model
+lr = LinearRegression()
+
+# Fit the model on the training set
+lr.fit(X_train, y_train)
+
+# Predict the target variable for the testing set
+y_pred = lr.predict(X_test)
+
+# Calculate the mean squared error and R-squared score
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print("Mean squared error:", mse)
+print("R-squared score:", r2)
