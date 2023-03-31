4e8f2e8999d50c5afdccbd48e9da6c425e4ff276
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2023-03-31 12:03:50 +0200

    created file exercises/9ddcc95c-9826-4408-be68-22550ee55bfc/solutions/4b7823f7-e52f-40e0-a527-e39b795a29d6/solution.py

---
+++ exercises/9ddcc95c-9826-4408-be68-22550ee55bfc/solutions/4b7823f7-e52f-40e0-a527-e39b795a29d6/solution.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+
+clf = LogisticRegression(max_iter=1000)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print('Accuracy:', accuracy)
