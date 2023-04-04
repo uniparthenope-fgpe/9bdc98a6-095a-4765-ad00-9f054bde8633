5774dccd8f248681bb8828edb8000e8ea8aa8b34
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2023-04-04 11:41:06 +0200

    created file exercises/78687766-f019-43d6-bab3-2cef13859b81/solutions/a0deac5c-8cf3-4708-bb8f-52113efed9ea/solution.py

---
+++ exercises/78687766-f019-43d6-bab3-2cef13859b81/solutions/a0deac5c-8cf3-4708-bb8f-52113efed9ea/solution.py
@@ -0,0 +1,9 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+rf.fit(X_train, y_train)
