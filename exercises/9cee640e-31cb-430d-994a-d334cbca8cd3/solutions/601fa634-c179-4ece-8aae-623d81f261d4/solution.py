47f47943a8b784b715fe96de7645485fe1f5b7b3
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2023-04-04 11:40:17 +0200

    created file exercises/9cee640e-31cb-430d-994a-d334cbca8cd3/solutions/601fa634-c179-4ece-8aae-623d81f261d4/solution.py

---
+++ exercises/9cee640e-31cb-430d-994a-d334cbca8cd3/solutions/601fa634-c179-4ece-8aae-623d81f261d4/solution.py
@@ -0,0 +1,9 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+
+dt = DecisionTreeClassifier(random_state=42)
+dt.fit(X_train, y_train)
