a039088cc2949db7c167005a9395da9fd321035d
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2023-04-04 11:31:14 +0200

    created file exercises/7c0fb59f-8e9e-4fa7-8f5b-1870e8c92329/solutions/b8174902-b0ed-4764-87de-26ac5afa1bc7/solution.py

---
+++ exercises/7c0fb59f-8e9e-4fa7-8f5b-1870e8c92329/solutions/b8174902-b0ed-4764-87de-26ac5afa1bc7/solution.py
@@ -0,0 +1,10 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
